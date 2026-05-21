ced84fcf7bfb4845f9e9f981c0a22bdb802c7acb
matt-macarte <matt.almeidac@gmail.com>
2026-05-20 22:29:38 -0300

    :sparkles: inicia ambiente para prática de shadow dom

---
+++ src/aula8.js
@@ -0,0 +1,17 @@
+import './style.css'
+import './aula8.css'
+
+// document.querySelector('#aula8').innerHTML = ``
+
+class Ranked extends HTMLElement {
+    constructor() {
+        
+        let stars = 0;
+    }
+    estrelinhas(){
+        return "EEEEE";
+    }
+    tipo(){
+
+    }
+}
